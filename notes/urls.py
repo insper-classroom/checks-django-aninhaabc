8a592dd72a1ef8a3d9dd57ad42619e0cd14ae0bb
Mona Lisa <aninhabiac123@gmail.com>
2023-11-14 14:14:30 -0300

    check 6 e 7

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('creditos/', views.credito, name='credito')
+    path('creditos/', views.credito, name='credito'),
+    path('anotacoes/<int:note_id>/apagar',views.delete_note, name='delete-note'),
+    path('anotacoes/<int:note_id>', views.edit_note, name='edit-note')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Note
 
 def index(request):
-    all_notes = Note.objects.all().order_by('created_at')
-    # html_response = '<h1>Get-it</h1><ul>'
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
 
-    # for note in all_notes:
-    #     html_response += f'<li>{note.title}</li>'
+        print(f'Titulo={title}\nConteudo={content}\n')
 
-    # html_response += '</ul>'
+        n = Note(title=title,content=content)
+        n.save()
+
+        return redirect('index')
     
-    return render(request, "notes/index.html" , {'notes' : all_notes})
+    else:
+        all_notes = Note.objects.all().order_by('-created_at')
+        return render(request, "notes/index.html" , {'notes' : all_notes})
+
+def delete_note(request, note_id):
+    note = Note.objects.get(id=note_id)
+    note.delete()
+    return redirect('index')
+
+def edit_note(request, note_id):
+    note = Note.objects.get(id=note_id)
+    if request.method =='POST':
+        note.title = request.POST.get('titulo')
+        note.content = request.POST.get('detalhes')
+        note.save()
+        return redirect('index')
+    else:
+        return render(request, "notes/index.html",{'note': note})
 
 def credito(request):
     return HttpResponse("<h1>Créditos</h1><p>Sistema web desenvolvido por Ana Beatriz.</p>")
