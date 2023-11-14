d5f5d7a85e1b3367b7ed9e2c6921c97204400958
Mona Lisa <aninhabiac123@gmail.com>
2023-11-14 09:26:42 -0300

    check 5

--- notes/views.py
+++ notes/views.py
@@ -6,14 +6,14 @@ from .models import Note
 
 def index(request):
     all_notes = Note.objects.all().order_by('created_at')
-    html_response = '<h1>Get-it</h1><ul>'
+    # html_response = '<h1>Get-it</h1><ul>'
 
-    for note in all_notes:
-        html_response += f'<li>{note.title}</li>'
+    # for note in all_notes:
+    #     html_response += f'<li>{note.title}</li>'
 
-    html_response += '</ul>'
+    # html_response += '</ul>'
     
-    return HttpResponse(html_response)
+    return render(request, "notes/index.html" , {'notes' : all_notes})
 
 def credito(request):
     return HttpResponse("<h1>Créditos</h1><p>Sistema web desenvolvido por Ana Beatriz.</p>")
