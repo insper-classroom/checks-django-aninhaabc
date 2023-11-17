c20c951d8cf0e3817096729486338b5d1a92ee86
Mona Lisa <aninhabiac123@gmail.com>
2023-11-17 09:32:11 -0300

    check 10

--- notes/models.py
+++ notes/models.py
@@ -2,10 +2,18 @@ from typing import Any
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 class Note(models.Model):
     title = models.CharField(max_length=70)
     content = models.TextField(blank=True,null=True)
     created_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
     
     def __str__(self):
         return f'{self.id} {self.title}'
--- notes/views.py
+++ notes/views.py
@@ -12,15 +12,18 @@ def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
+        author = request.user
 
-        n = Note(title=title,content=content)
+        n = Note(title=title,content=content,author=author)
         n.save()
 
         return redirect('index')
     
     else:
         all_notes = Note.objects.all().order_by('-created_at')
-        return render(request, "notes/index.html" , {'notes' : all_notes})
+        usuario_selecionado = request.user
+        anotacoes_do_usuario = Note.objects.filter(author=usuario_selecionado).order_by('-created_at')
+        return render(request, "notes/index.html" , {'notes' : anotacoes_do_usuario})
 
 def delete_note(request, note_id):
     note = Note.objects.get(id=note_id)
